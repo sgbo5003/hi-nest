87dbc197dab8865781c5531b7d4d9d36ebccb7b3
sgbo5003 <61876422+sgbo5003@users.noreply.github.com>
2022-06-03 17:41:35 +0900

    #2.3 Movies Service part Two
    about update(patch) and delete

--- src/movies/movies.service.ts
+++ src/movies/movies.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Movie } from './entities/movie.entity';
 
 @Injectable()
@@ -10,18 +10,28 @@ export class MoviesService {
   }
 
   getOne(id: string): Movie {
-    return this.movies.find(movie => movie.id === parseInt(id));
+    const movie = this.movies.find(movie => movie.id === parseInt(id));
+    if(!movie) {
+      throw new NotFoundException(`Movie with ID ${id} not found.`);
+    }
+    return movie;
   }
 
-  deleteOne(id: string): boolean {
-    this.movies.filter(movie => movie.id !== parseInt(id));
-    return true;
+  deleteOne(id: string) {
+    this.getOne(id);
+    this.movies = this.movies.filter(movie => movie.id !== parseInt(id));
   }
 
   create(movieData) {
     this.movies.push({
       id: this.movies.length + 1,
-      ...movieData
+      ...movieData,
     });
+  }
+
+  update(id: string, updateData) {
+    const movie = this.getOne(id);
+    this.deleteOne(id);
+    this.movies.push({...movie, ...updateData});
   }
 }
